2cc8529239a70912c8a440dcf5e5db8ceabb9c63
EpamelisAosinery <hxho@hawkmail.hfcc.edu>
2020-05-19 15:02:46 -0400

    Changing files

--- hw1-1.py
+++
@@ -1,8 +0,0 @@
-############################
-# Author:    Epamelis Aosinery
-# Date:        1/17/2020
-# Desr:         Homework 1-1
-############################
-fruit = input("PLease enter name of fruit: ")
-cost = float(input("Please enter cost per item: "))
-print(fruit + "'s are $%.2f" % cost, "each.")
